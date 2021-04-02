c308f2ca3c3f9ec1e5de6a2b8b315c4261d747ff
Gerryl Esperacion <GerrylEspreacion@gmail.com>
2021-04-02 13:48:21 -0700

    winning conditons added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,6 +9,17 @@
 
 //Write down all the winning combinations
 
+const winningCondition = [
+    [0,1,2],  //Horizational win condition
+    [3,4,5],
+    [6,7,8],
+    [0,3,6], //vertical win condition
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],//Diagonal win condition
+    [2,4,6]
+] 
+
 
 //Create a function that checks is the winning conditions have been met 
 //Display winner
