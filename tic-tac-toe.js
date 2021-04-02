3c89c2e4cda41073d4e87802cb9fb908a07f60e8
Gerryl Esperacion <GerrylEspreacion@gmail.com>
2021-04-02 13:45:49 -0700

    position clicker

---
+++ tic-tac-toe.js
@@ -0,0 +1,21 @@
+//Write a function that tracks the clicks on the page 
+// Write a code that can track when each tile of the tic tac toe is clicked
+    window.addEventListener("DOMContentLoaded", event =>{
+        document.addEventListener('click',(event) => {
+            console.log(event.target.id)
+        })
+    })
+//When clicked, a mark should be placed/ check for a mark    
+
+//Write down all the winning combinations
+
+
+//Create a function that checks is the winning conditions have been met 
+//Display winner
+
+
+// Function buttons
+//    If Give up is writted, player loses 
+// New Game just reloads the page
+
+
